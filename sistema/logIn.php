9e4d7fdb5c1951d30d47249665632c8dc8c61563
Angel Banze <angeleliasanjo@gmail.com>
2022-05-03 05:39:18 -0700

    conectando a base de dados

--- sistema/logIn.php
+++ sistema/logIn.php
@@ -1,25 +1,35 @@
 <?php
     session_start();
+    //controlando acesso
     $_SESSION['dentro'] = False;
 
-    $usuario = '1247854687122';
-    $senha = '1111';
-
+    //pegando dados do usuario na pagina index.php
     $p_usuario = $_POST['usuario'] ?? NULL;
     $p_senha = $_POST['senha'] ?? NULL;
 
-    if ($p_usuario == $usuario && $p_senha == $senha) {
-        $_SESSION['usuario'] = $usuario;
-        $_SESSION['senha'] = $senha;
-        $_SESSION['dentro'] = True;
-    }
-?>
+    
+    //evitar ataques do tipo  sqlInjection
+    $p_usuario = stripcslashes($p_usuario);
+    $p_senha = stripcslashes($p_senha);
+    $p_usuario = mysql_real_escape_string($p_usuario);
+    $p_senha = mysql_real_escape_string($p_senha);
 
-<?php
-    if ($_SESSION['dentro'] == True) {
-        header("Location: pagina_inicial.php");
-    } else{
-        header("Location: index.php");
-        echo ('Usuario ou senha invalida');
+    //conexao a base de dados
+    mysql_connect("localhost", "root", "");
+    mysql_select_db("cliente_log");
+
+
+
+    //pesquisa pelo usuario na base de dados
+    $resultado = mysql_query ("select * from users where username = '$p_usuario' and password = '$p_senha'")
+                or die ("dados incorrectos ".mysqli_error());
+    
+    $colunas = mysqli_fetch_array($resultado);
+
+    if ($colunas['username'] == $p_usuario && $colunas['password'] == $p_senha) {
+        header('location: pagina_inicial.php');
+    } else {
+        header ('location: index.php');
     }
+    
 ?>
